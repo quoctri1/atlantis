8e370a43348b7967608ef6c429f3e0f552e2616c
Tri Phan <tri@yojee.com>
2023-05-03 16:31:12 +0700

    Add s3 bucket

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
-resource "null_resource" "example" {}
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.65.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
